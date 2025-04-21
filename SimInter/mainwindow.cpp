c351ad55c8cc4c928107225b380b08f9076fa597
AlexisAlvaradoB <luialvarado@estudiantec.cr>
2025-04-21 09:45:14 -0600

    Carga y salvado de Archivos

--- SimInter/mainwindow.cpp
+++ SimInter/mainwindow.cpp
@@ -2,6 +2,9 @@
 #include "ui_mainwindow.h"
 #include <QTextEdit>
 #include <QLayout>
+#include <QFileDialog>
+#include <fstream>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -12,18 +15,66 @@ MainWindow::MainWindow(QWidget *parent)
 
     for(int i = 0; i < 8; i++){
         this->PETabs[i] = new QWidget(ui->loadMemoryWidget);
-        ui->PEMemoriesTW->addTab(this->PETabs[i], QString("PE ").append(std::to_string(i)));
+        ui->PEMemoriesTW->addTab(this->PETabs[i], QString("PE ").append(std::to_string(i + 1)));
         this->PETextEdits[i] = new QTextEdit();
-        this->PETextEdits[i]->setText(QString("PE ").append(std::to_string(i)).append(" worked"));
-
         QVBoxLayout *layout = new QVBoxLayout(this->PETabs[i]);
         layout->addWidget(this->PETextEdits[i]);
         this->PETabs[i]->setLayout(layout);
 
+
+
     }
+    connect(ui->actionSavePE, &QAction::triggered, this, &MainWindow::onActionSavePETriggered);
+    connect(ui->actionSaveAllPEs, &QAction::triggered, this, &MainWindow::onActionSaveAllPEsTriggered);
+    connect(ui->actionCargarMemorias, &QAction::triggered, this, &MainWindow::onActionCargarMemoriasTriggered);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::onActionSavePETriggered()
+{
+    if(this->path.length() > 0){
+        int tab = this->ui->PEMemoriesTW->currentIndex();
+        std::ofstream PEFile(QString("../../InstructionsFile/InstructionsP").append(std::to_string(tab + 1)).append(".txt").toStdString());
+        if(PEFile.is_open()){
+            PEFile  << this->PETextEdits[tab]->toPlainText().toStdString();
+            PEFile.close();
+        }
+        else{
+            qDebug() << "Failed to write";
+        }
+    }else{
+
+    }
+}
+
+void MainWindow::onActionSaveAllPEsTriggered()
+{
+    qDebug() <<"Clicked 2" ;
+}
+
+void MainWindow::onActionCargarMemoriasTriggered(){
+    this->path = QFileDialog::getExistingDirectory(this, QString("Seleccione la carpeta con Instrucciones"), QDir::currentPath());
+    this->cargarArchivosMemoria(this->path);
+}
+
+void MainWindow::cargarArchivosMemoria(QString path){
+    //Carga los archivos de las instrucciones en el espacio de texto:
+    std::string textInLine;
+    for(int i = 0; i < 8; i++){
+        QString textInTextEdit = "";
+        QString pathToFile = this->path;
+        pathToFile.append("/InstructionsP").append(std::to_string(i + 1)).append(".txt");
+
+        std::ifstream PEFile(pathToFile.toStdString());
+        while(std::getline(PEFile, textInLine)){
+            textInTextEdit.append(textInLine).append("\n");
+        }
+        this->PETextEdits[i]->setText(textInTextEdit);
+        PEFile.close();
+    }
 }
--- SimInter/mainwindow.h
+++ SimInter/mainwindow.h
@@ -18,9 +18,16 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void onActionSavePETriggered();
+    void onActionSaveAllPEsTriggered();
+    void onActionCargarMemoriasTriggered();
+
 private:
     Ui::MainWindow *ui;
     QWidget * PETabs [8];
     QTextEdit * PETextEdits[8];
+    QString path;
+    void cargarArchivosMemoria(QString path);
 };
 #endif // MAINWINDOW_H
