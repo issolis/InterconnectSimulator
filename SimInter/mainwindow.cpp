bc3ce202c28cbc0517bcde8ac970c78bc044c392
AlexisAlvaradoB <luialvarado@estudiantec.cr>
2025-04-20 20:43:33 -0600

    Basic PE Tabs

--- SimInter/mainwindow.cpp
+++ SimInter/mainwindow.cpp
@@ -1,11 +1,26 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QTextEdit>
+#include <QLayout>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    //Crear las tabs para la memoria
+
+    for(int i = 0; i < 8; i++){
+        this->PETabs[i] = new QWidget(ui->loadMemoryWidget);
+        ui->PEMemoriesTW->addTab(this->PETabs[i], QString("PE ").append(std::to_string(i)));
+        this->PETextEdits[i] = new QTextEdit();
+        this->PETextEdits[i]->setText(QString("PE ").append(std::to_string(i)).append(" worked"));
+
+        QVBoxLayout *layout = new QVBoxLayout(this->PETabs[i]);
+        layout->addWidget(this->PETextEdits[i]);
+        this->PETabs[i]->setLayout(layout);
+
+    }
 }
 
 MainWindow::~MainWindow()
--- SimInter/mainwindow.h
+++ SimInter/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTextEdit>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -19,5 +20,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QWidget * PETabs [8];
+    QTextEdit * PETextEdits[8];
 };
 #endif // MAINWINDOW_H
